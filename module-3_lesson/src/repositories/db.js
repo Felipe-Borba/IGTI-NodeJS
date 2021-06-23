f564ad5cfc951860efdbb017d151c8ebe858685d
Felipe Borba <felipe.borba0109@gmail.com>
2021-06-23 12:15:52 -0300

    change db credentials

--- module-3_lesson/src/repositories/db.js
+++ module-3_lesson/src/repositories/db.js
@@ -7,7 +7,7 @@ async function connect() {
 
   const pool = new pg.Pool({
     connectionString:
-      "postgres://obwrxdnl:n82rOxHh9I3P2ZU2xjmEobjBQE-KSSe7@tuffi.db.elephantsql.com/obwrxdnl",
+      "https://api.elephantsql.com/console/55e57472-0090-49c7-8aa2-b96925f956bf/details",
   });
 
   global.connection = pool;
